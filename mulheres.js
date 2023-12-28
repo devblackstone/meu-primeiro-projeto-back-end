9b4d04968492379c25b546499158e1941585eda4
devblackstone <115595627+devblackstone@users.noreply.github.com>
2023-12-28 15:58:05 -0300

    Update mulheres.js

--- mulheres.js
+++ mulheres.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const app = express()
 const porta = 3333
-const Mulheres = {
+const mulheres = {
         nome: 'Simara Conceição',
 
         imagem: 'https://bit.ly/3LJIyOF',
